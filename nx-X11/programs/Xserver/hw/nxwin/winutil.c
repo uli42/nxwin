e6a7eeec306475862305c11b4a0e39f771c6ce7d
Ulrich Sibiller <uli42@gmx.de>
2020-01-30 00:08:46 +0100

    import of nxwin-2.1.0-2.tar.gz

--- nx-X11/programs/Xserver/hw/nxwin/winutil.c
+++ nx-X11/programs/Xserver/hw/nxwin/winutil.c
@@ -2,7 +2,7 @@
 /*                                                                        */
 /* Copyright (c) 2001,2006 NoMachine, http://www.nomachine.com.           */
 /*                                                                        */
-/* NXPROXY, NX protocol compression and NX extensions to this software    */
+/* NXWIN, NX protocol compression and NX extensions to this software      */
 /* are copyright of NoMachine. Redistribution and use of the present      */
 /* software is allowed according to terms specified in the file LICENSE   */
 /* which comes in the source distribution.                                */
